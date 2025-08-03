9314e3143c936aad5d3754ad77e4a2e899f7b59a
Ehab Farid <ehab.mahmoud_a287@fci.kfs.edu.eg>
2025-08-04 01:31:34 +0300

    Delete functiontask directory

--- functiontask/mariopylist1.py
+++
@@ -1,7 +0,0 @@
-def mario_list():
-    l = [" ", " ", " ", " ", " "]
-    for i in l:
-        l.append("*")
-        l.remove(" ")
-        print(l)
-mario_list()
--- functiontask/marippy2.py
+++
@@ -1,5 +0,0 @@
-def print_mariopyr(n):
-    for i in range(n + 1):
-        print(" " * (n - i) + "*" * i)
-
-print_mariopyr(4)
