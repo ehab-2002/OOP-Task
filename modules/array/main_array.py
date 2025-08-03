aac81e5bca80dd8cd6aa8f4cc74f0b2abc7bd329
Ehab Farid <ehab.mahmoud_a287@fci.kfs.edu.eg>
2025-08-04 01:31:50 +0300

    Delete modules directory

--- modules/array/main_array.py
+++
@@ -1,5 +0,0 @@
-from array_making import find_i_locations
-find_i_locations()
-
-from array_making import sort_numbers
-sort_numbers()
--- modules/module_mariopy/main_mario.py
+++
@@ -1,7 +0,0 @@
-from mario import print_mariopyr
-print_mariopyr(5)
-
-
-
-from mario import mario_list
-mario_list()
